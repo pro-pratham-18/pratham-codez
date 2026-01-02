8d752440d048f35e19443d6cc8ef804ce23f688d
pro-pratham-18 <itz.prathathamesh18@gmail.com>
2026-01-02 20:25:03 +0530

    Create proprtham.cpp

---
+++ proprtham.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+
+
+int main(){
+  count=0;
+  int n;
+  cout<<"Enter the number :";
+  cin>>n;
+  for(int i=1;i<=n;i++){
+    count+=i;
+  }
+
+  cout<<"The sum is:"<<count;
+}
